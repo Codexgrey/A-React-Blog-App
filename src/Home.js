af0a7bb1833e703db387ed42e42df9765e261343
Codexgrey <thecodexgrey@gmail.com>
2021-09-21 17:55:52 +0100

    created custom hook

--- src/Home.js
+++ src/Home.js
@@ -1,22 +1,14 @@
-import { useState } from "react";
 import Bloglist from "./Bloglist";
+import useFetch from "./useFetch";
 
 const Home = () => {  
-    const [blogs, setBlogs] = useState([
-        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-    ]); 
-
-    const handleDelete = (id) => {
-      const newBlogs = blogs.filter(blog => blog.id !== id);
-      setBlogs(newBlogs);
-    }
-
+    const { data: blogs, loading, error } = useFetch('http://localhost:8000/blogs');
+ 
     return ( 
         <div className="home">
-          <Bloglist blogs={blogs} title="All Blogs" handleDelete={handleDelete}/>
-          {/* <Bloglist blogs={blogs.filter((blog) => blog.author === 'yoshi')} title= "Yoshi's Blogs" /> */}
+          { error &&  <div>{ error }</div>}
+          { loading && <div>loading...</div> }
+          {blogs && <Bloglist blogs={blogs} title="All Blogs"/>}
         </div>
      );
 }
