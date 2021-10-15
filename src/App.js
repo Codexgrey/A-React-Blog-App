2235ee6f12534da8023b303bb2cf690d41158311
Codexgrey <thecodexgrey@gmail.com>
2021-10-15 19:43:20 +0100

    setup Router and Home Route

--- src/App.js
+++ src/App.js
@@ -4,13 +4,19 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
    return (
-    <div className="App">
-      <Navbar />
-      <div className="content">
-        <Home />
-      </div>
-    </div> 
+    <Router>
+      <div className="App">
+        <Navbar /> 
+        <div className="content">
+          <Switch>
+             <Route path="/"> <Home /> </Route>
+          </Switch>
+        </div>
+      </div> 
+    </Router>
   );
 }
 
 export default App;
+
+// The Navbar Comp is nested inside the Router Comp & is always gonna show up in each page, as it isn't nested inside the Switch Comp as well
